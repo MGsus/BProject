d542bb5043a9e9081992901656c80967f4ea5d3f
Jesús Orlando Montoya Mejía <jesus.orlando.montoya.mejia@ibm.com>
2021-01-05 11:01:48 -0500

    URIs updated

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -20,7 +20,7 @@ export class AuthService {
     else return of(this.user);
   }
 
-  private isLoggedURL = 'http://169.57.42.77:31064/auth/logged';
+  private isLoggedURL = 'http://169.57.42.77:30937/auth/logged';
   checkAuth(): Observable<User> {
     console.log('calling auth service');
     let req = this.http.get<User>(this.isLoggedURL, { withCredentials: true });
--- src/app/services/bp.service.ts
+++ src/app/services/bp.service.ts
@@ -10,32 +10,32 @@ export class BpService {
   constructor(private http: HttpClient) {}
 
   getBps(): Observable<any> {
-    return this.http.get<any>(`http://169.57.42.77:31064/api/bp/list`, {
+    return this.http.get<any>(`http://169.57.42.77:30937/api/bp/list`, {
       withCredentials: true,
     });
   }
 
   getBpByName(name: string): Observable<Bp> {
-    return this.http.get<Bp>(`http://169.57.42.77:31064/api/bp/${name}`, {
+    return this.http.get<Bp>(`http://169.57.42.77:30937/api/bp/${name}`, {
       withCredentials: true,
     });
   }
 
   newBP(body: Bp): Observable<Bp> {
-    return this.http.post<Bp>(`http://169.57.42.77:31064/api/bp/insert`, body, {
+    return this.http.post<Bp>(`http://169.57.42.77:30937/api/bp/insert`, body, {
       withCredentials: true,
     });
   }
 
   updateBP(bp: Bp): Observable<any> {
-    return this.http.put(`http://169.57.42.77:31064/api/bp/update`, bp, {
+    return this.http.put(`http://169.57.42.77:30937/api/bp/update`, bp, {
       withCredentials: true,
     });
   }
 
   insertCert(bpNombre: String, cert: Cert): Observable<any> {
     return this.http.put(
-      `http://169.57.42.77:31064/api/bp/update`,
+      `http://169.57.42.77:30937/api/bp/update`,
       { bp_nombre: bpNombre, bp_cert: cert },
       {
         withCredentials: true,
@@ -44,13 +44,13 @@ export class BpService {
   }
 
   getOpps(): Observable<any> {
-    return this.http.get(`http://169.57.42.77:31064/api/bp/list`, {
+    return this.http.get(`http://169.57.42.77:30937/api/bp/list`, {
       withCredentials: true,
     });
   }
   insertOpp(bpNombre: String, opp: Opp): Observable<any> {
     return this.http.put(
-      `http://169.57.42.77:31064/api/bp/insertOpp`,
+      `http://169.57.42.77:30937/api/bp/insertOpp`,
       {
         bp_nombre: bpNombre,
         bp_opp: opp,
@@ -61,7 +61,7 @@ export class BpService {
 
   updateOpp(bpNombre: String, opp: Opp): Observable<any> {
     return this.http.put(
-      `http://169.57.42.77:31064/api/bp/updateOpp`,
+      `http://169.57.42.77:30937/api/bp/updateOpp`,
       { bp_nombre: bpNombre, bp_opp: opp },
       { withCredentials: true }
     );
